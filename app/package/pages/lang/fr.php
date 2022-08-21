a161f395c54e82e77ad276525e18e8833b1acc57
Emilien52 <emiliengarandeau@yahoo.fr>
2022-08-21 22:09:52 +0200

    Update fr.php

--- app/package/pages/lang/fr.php
+++ app/package/pages/lang/fr.php
@@ -1,21 +1,30 @@
 <?php
 
-const PAGES_ADD_TITLE = "Pages | Créer";
-const PAGES_ADD_DESC = "Créer une nouvelle page";
+return [
+	"add" => [
+		"title" => "Pages | Créer",
+		"desc" => "Créer une nouvelle page",
+	],
+	"edit" => [
+		"title" => "Pages | Édition",
+		"desc" => "Modifiez votre page",
+	],
+	"list" => [
+		"title" => "Pages | Liste",
+		"desc" => "Liste des pages du site",
+	],
+	"publish" => "Publication de la page",
+	"draft" => "Brouillon",
 
-const PAGES_EDIT_TITLE = "Pages | Édition";
-const PAGES_EDIT_DESC = "Modifiez votre page";
+	"title" => "Titre",
+	"author" => "Auteur",
 
-const PAGES_ADD_CARD_TITLE = "Contenu de la page";
+	"creation" => [
+		"date" => "Date de création",
+	],
+	"content" => [
+		"placeholder" => "Commencez à taper ou cliquez sur le \"+\" pour choisir un bloc à ajouter...",
+	],
 
-const PAGES_LIST_TITLE = "Pages | Liste";
-const PAGES_LIST_DESC = "Liste des pages du site";
 
-const PAGES_PUBLISH = "Publication de la page";
-const PAGES_DRAFT = "Brouillon";
-
-const PAGES_TITLE = "Titre";
-const PAGES_AUTHOR = "Auteur";
-const PAGES_CREATION_DATE = "Date de création";
-
-const PAGES_CONTENT_PLACEHOLDER = "Commencez à taper ou cliquez sur le \"+\" pour choisir un bloc à ajouter...";
+];
