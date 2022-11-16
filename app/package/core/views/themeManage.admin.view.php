70fb35d8700c2a09975ba0d239700c33f27ebae9
Léo <69589034+Zomblard@users.noreply.github.com>
2022-11-16 21:41:56 +0100

    [EDIT] theme manage view

--- app/package/core/views/themeManage.admin.view.php
+++ app/package/core/views/themeManage.admin.view.php
@@ -6,10 +6,18 @@ use CMW\Manager\Lang\LangManager;
 $title = LangManager::translate("core.theme.manage.title", ["theme" => ThemeController::getCurrentTheme()->getName()]);
 $description = LangManager::translate("core.theme.manage.description"); ?>
 
-
-<!-- THEME CONFIG FILE -->
-<div class="content">
-    <div class="container-fluid">
-        <?php ThemeController::getCurrentThemeConfigFile(); ?>
-    </div>
+<div class="d-flex flex-wrap justify-content-between">
+    <h3><i class="fa-solid fa-pen-nib"></i> <span class="m-lg-auto">Apparence de <b><?= ThemeController::getCurrentTheme()->getName() ?></b></span></h3>
+    <div class="buttons"><button form="ThemeSettings" type="submit" class="btn btn-primary">Sauvegarder</button></div>
 </div>
+<!-- THEME CONFIG FILE -->
+
+<form id="ThemeSettings" action="" method="post">
+    <div class="row">
+        <div class="card">
+            <div class="card-body">
+                <?php ThemeController::getCurrentThemeConfigFile(); ?>
+            </div>
+        </div>
+    </div>
+</form>
