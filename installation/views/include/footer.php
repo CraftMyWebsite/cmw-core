b23aaf4d26319610b94741c387ee52dbe394f866
Léo <69589034+Zomblard@users.noreply.github.com>
2023-04-25 20:17:55 +0200

    [ADD] Correction front installer
    Toggle show / hide password
    scipt js qui indique la force du mdp admin
    script qui check si les 2 mdp sont bons
    scrollbar custom à nos couleurs
    footer avec les copyright
    Revue front global de la page themes

--- installation/views/include/footer.php
+++ installation/views/include/footer.php
@@ -1,4 +1,21 @@
             </div>
         </div>
     </body>
+    <div class="text-sm mt-auto xl:px-64">
+    <div class="flex flex-wrap  items-center">
+        <div class="px-6 py-6 md:flex-1">
+            <p>Copyright © <a class="text-cmw-pink" href="https://craftmywebsite.fr/" target="_blank">CraftMyWebsite</a></b></p>
+        </div>
+        <div class="px-4 py-2 w-full sm:w-auto">
+            <div class="flex-wrap inline-flex space-x-3">
+                <a href="https://craftmywebsite.fr/r/install-discord" target="_blank" class="hover:text-blue-600">
+                    <i class="fa-xl fa-brands fa-discord"></i>
+                </a>
+                <a href="https://craftmywebsite.fr/r/install-twitter" target="_blank" class="hover:text-blue-600">
+                    <i class="fa-xl fa-brands fa-twitter"></i>
+                </a>
+            </div>
+        </div>
+        </div>
+    </div>
 </html>
--- installation/views/sixInstall.view.php
+++ installation/views/sixInstall.view.php
@@ -1,7 +1,7 @@
 <?php use CMW\Manager\Lang\LangManager; ?>
 
 <h2 class="text-2xl font-medium text-center"><?= LangManager::translate("installation.administrator.title") ?></h2>
-<form action="installer/submit" method="post" id="mainForm">
+<form action="installer/submit" method="post" id="mainForm" name="mainForm">
     <div class="lg:grid grid-cols-2 gap-8">
         <div class="form-control">
             <p><?= LangManager::translate("users.users.pseudo") ?> :</p>
@@ -24,24 +24,118 @@
             <p><?= LangManager::translate("users.users.password") ?> :</p>
             <label class="input-group input-group">
                 <span><i class="fa-solid fa-unlock"></i></span>
-                <input type="password" placeholder="<?= LangManager::translate("users.users.pass") ?>"
+                <input onblur="checkPasswordIsSame()" type="password" placeholder="<?= LangManager::translate("users.users.pass") ?>"
                        id="password" name="password"
                        class="input input-bordered input-sm w-full" required>
+                       <div onclick="showPassword()" class="cursor-pointer py-1 px-2 text-sm font-medium text-white bg-cmw-pink rounded-r-lg"><i class="fa fa-eye-slash"></i></div>
             </label>
         </div>
         <div class="form-control">
             <p><?= LangManager::translate("users.users.password_confirm") ?> :</p>
             <label class="input-group input-group">
                 <span><i class="fa-solid fa-unlock"></i></span>
-                <input type="password" placeholder="<?= LangManager::translate("users.users.pass") ?>"
+                <input onblur="checkPasswordIsSame()" type="password" placeholder="<?= LangManager::translate("users.users.pass") ?>"
                        id="passwordCheck" name="passwordCheck"
                        class="input input-bordered input-sm w-full" required>
+                       <div onclick="showPasswordV()" class="cursor-pointer py-1 px-2 text-sm font-medium text-white bg-cmw-pink rounded-r-lg"><i class="fa fa-eye-slash"></i></div>
             </label>
         </div>
     </div>
+    <div class="mt-2">Force du mot de passe :
+        <div class="">
+            <progress class="w-1/2" max="100" value="0" id="meter"></progress>
+        </div>
+    </div>
+    <div class="mt-2" id="passwordTextAlert"></div>
     <div class="card-actions justify-end">
         <button id="formBtn" type="submit" class="btn btn-primary">
             <?= LangManager::translate("core.btn.next") ?>
         </button>
     </div>
 </form>
+
+<script>
+
+function showPassword() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+    function showPasswordV() {
+        var x = document.getElementById("passwordCheck");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+
+    function checkPasswordIsSame() {
+        let textAlert = document.getElementById("passwordTextAlert");
+        var pass1 = document.forms["mainForm"]["password"].value;
+        var pass2 = document.forms["mainForm"]["passwordCheck"].value;
+        textAlert.style.display = 'none';
+        if (pass2) {
+            textAlert.style.display = 'inline-block';
+            if (pass1 == pass2) {
+                textAlert.style.display = 'none';
+                document.getElementById("formBtn").disabled = false;
+            } else {
+                textAlert.innerHTML = "<p class='text-cmw-pink'>Les mot de passe ne corespondent pas !</p>";
+                document.getElementById("formBtn").disabled = true;
+            }
+        }
+    }
+
+    //Check password strenght
+
+    let code = document.getElementById("password");
+
+    let strengthbar = document.getElementById("meter");
+
+    code.addEventListener("keyup", function(){checkpassword(code.value)
+
+    })
+
+    function checkpassword(password)
+    {
+        let strength=0;
+        if (password.match(/[a-z]+/)){
+            strength+=1;
+        }
+        if (password.match(/[A-Z]+/)){
+            strength+=1;
+        }
+        if (password.match(/[0-9]+/)){
+            strength+=1;
+        }
+        if (password.match(/[$@#&!]+/)){
+            strength+=1;
+
+        }
+        switch(strength){
+            case 0:
+                strengthbar.value=0;
+                break;
+
+            case 1:
+                strengthbar.value=25;
+                break;
+
+            case 2:
+                strengthbar.value=50;
+                break;
+
+            case 3:
+                strengthbar.value=75;
+                break;
+
+            case 4:
+                strengthbar.value=100;
+                break; }
+    }
+
+</script>
