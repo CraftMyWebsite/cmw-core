6ae5d4ddb0dbf6e512a4c3d945556a93780a81e1
Thomas Texier <thomas_texier@connect-tech.sncf>
2023-05-07 13:00:08 +0200

    [FIX] Fix admin darkMode

--- Admin/Resources/Views/Includes/head.inc.php
+++ Admin/Resources/Views/Includes/head.inc.php
@@ -83,5 +83,6 @@ use CMW\Utils\Utils;
 <script>
     const theme = localStorage.getItem('theme') || 'light';
     document.body.className = theme;
+    console.log(theme)
   </script>
 <div id="app">
