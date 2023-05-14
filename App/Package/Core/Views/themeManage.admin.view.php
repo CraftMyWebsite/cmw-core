64444d4700b88c89fea6d7bb3bc72bf93e4147cf
Léo <69589034+Zomblard@users.noreply.github.com>
2023-05-14 12:20:05 +0200

    [ADD] reset button in config theme

--- App/Package/Core/Views/themeManage.admin.view.php
+++ App/Package/Core/Views/themeManage.admin.view.php
@@ -7,10 +7,19 @@ use CMW\Manager\Security\SecurityManager;
 $title = LangManager::translate("core.Theme.manage.title", ["Theme" => ThemeController::getCurrentTheme()->getName()]);
 $description = LangManager::translate("core.Theme.manage.description"); ?>
 
-<div class="d-flex flex-wrap justify-content-between">
+<div class="d-flex flex-wrap justify-content-between" style="width: 100%">
     <h3><i class="fa-solid fa-pen-nib"></i> <span class="m-lg-auto">Apparence de <b><?= ThemeController::getCurrentTheme()->getName() ?></b></span></h3>
-    <div class="buttons"><button form="ThemeSettings" type="submit" class="btn btn-primary">Sauvegarder</button></div>
+    <div class="d-flex flex-wrap justify-content-end">
+        <form class="me-4" action="market/regenerate" method="post">
+            <?php (new SecurityManager())->insertHiddenToken() ?>
+            <button type="submit" class="btn btn-warning ">Réinitialiser</button>
+        </form>
+        <div>
+            <button form="ThemeSettings" type="submit" class="btn btn-primary">Sauvegarder</button>
+        </div>
+    </div>
 </div>
+
 <!-- THEME CONFIG FILE -->
 
 <form id="ThemeSettings" action="" method="post" enctype="multipart/form-data">
