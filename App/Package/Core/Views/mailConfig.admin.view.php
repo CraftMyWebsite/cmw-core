eef4bbd63717c553555ae3f0924f088f1c2670a3
Léo <69589034+Zomblard@users.noreply.github.com>
2023-05-15 23:35:16 +0200

    [FIX] tinymce class

--- App/Package/Core/Views/mailConfig.admin.view.php
+++ App/Package/Core/Views/mailConfig.admin.view.php
@@ -129,7 +129,7 @@ $description = LangManager::translate("core.mail.config.description");
                         </div>
                     </div>
                     <h6><?= LangManager::translate("core.mail.config.footer") ?> :</h6>
-                    <textarea name="footer"></textarea>
+                    <textarea class="tinymce" name="footer"></textarea>
                 </form>
             </div>
         </div>
