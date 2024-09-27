5df25e22d95c2a99a68e8c61f3427fdf194df477
Léo <69589034+Zomblard@users.noreply.github.com>
2024-09-27 16:05:13 +0200

    [FIX] Alert type

--- App/Manager/Flash/Alert.php
+++ App/Manager/Flash/Alert.php
@@ -9,7 +9,7 @@ class Alert
     public const string SUCCESS = 'success';
     public const string ERROR = 'error';
     public const string WARNING = 'warning';
-    public const string INFO = 'warning';
+    public const string INFO = 'info';
 
     public function __construct(
         #[ExpectedValues(flagsFromClass: Alert::class)]
