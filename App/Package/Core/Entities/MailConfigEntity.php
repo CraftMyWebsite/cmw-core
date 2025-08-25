9d69babcad1528b7209410feb99cf56ba8c2698a
zomb <leo.goigoux@gmail.com>
2025-08-25 15:26:17 +0200

    [FIX] getBody entity

--- App/Package/Core/Entities/MailConfigEntity.php
+++ App/Package/Core/Entities/MailConfigEntity.php
@@ -116,6 +116,9 @@ class MailConfigEntity extends AbstractEntity
      */
     public function getBody(): ?string
     {
+        if (!isset($this->body) || !is_null($this->body)) {
+            return null;
+        }
         return $this->body;
     }
 
